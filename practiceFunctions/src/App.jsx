7dd2b60dee28c21779f461035cab9e527aed9741
SAI KUMAR BELDARI <saikumarbeldari@gmail.com>
2023-12-07 17:59:56 +0530

    Social Platform

--- practiceFunctions/src/App.jsx
+++ practiceFunctions/src/App.jsx
@@ -3,13 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Sidebar from "../components/sidebar";
+import Post from "../components/Post";
+import CreatePost from "../components/createPost";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setselectedTab] = useState("Home");
+
   return (
     <div className="App">
-      <Sidebar></Sidebar>
+      <Sidebar
+        selectedTab={selectedTab}
+        setselectedTab={setselectedTab}
+      ></Sidebar>
       <div className="Post">
         <Header></Header>
+        {selectedTab === "Home" ? <CreatePost /> : <Post></Post>}
         <Footer></Footer>
       </div>
     </div>
